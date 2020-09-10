d15c22b4cbdaa1f9dbd2abfc33596f3823e332e3
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-09-10 15:21:28 +0200

    kommentera uppgift 2

--- uppgift2.py
+++ uppgift2.py
@@ -1,2 +1,12 @@
+
+# MÅL = UTTRYCK
+# .. betyder lagra värdet som nås när uttrycket i "UTTRYCK" beräknas
+# i variabeln som namnges i MÅL
+
+# input(...) ger en prompt till användaren att skriva något, och returnerar
+# textresultatet (en string/sträng)
+
 name = input("What is your name?")
+
+# f-string är trevligt för att skriva ut något på skärmen.
 print(f"Your name is: {name}")
