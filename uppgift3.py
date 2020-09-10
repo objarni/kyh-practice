65d16e5b1e8f476a7362bfc5c1f0b9fb8f530568
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-09-10 15:18:53 +0200

    kommentera uppgift 3

--- uppgift3.py
+++ uppgift3.py
@@ -1,18 +1,31 @@
+# Importera den inbyggda modulen "random", d.v.s gör randint m.fl
+# tillgängliga i detta script
 import random
 
+# Ta fram ett heltal ur 1, 2, 3, ... , 20 slumpmässigt
 n = random.randint(1, 20)
+
 print("I'm thinking of a number between 1 and 20. Guess which?")
 
+# Loopa för evigt... alltså tills "break" nås  :)
 while True:
+
+    # Ta in sträng från användaren
     text = input("Your guess: ")
+
+    # (Försök) Konvertera sträng till heltal
+    # str -> int
     as_number = int(text)
 
+    # Är användarens tal det datorn "tänker" på?
     if as_number == n:
         print("Correct!")
+        # break bryter ur while-loopen, vilket avslutar programmet
+        # eftersom det inte finns någon mer kod nedanför while-loopen
         break
 
+    # Ej lika med, så större eller mindre än.
     if as_number < n:
         print("Wrong, my number is higher... Try again!")
-
     if as_number > n:
         print("Wrong, my number is lower... Try again!")
