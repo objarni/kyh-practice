e79eaabcd36bd276dd6cbe432751a6021b59e12c
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-09-10 15:24:05 +0200

    kommentera uppgift 4

--- uppgift4.py
+++ uppgift4.py
@@ -2,11 +2,15 @@ import random
 
 n = random.randint(1, 100)
 print("Jag tänker på ett tal mellan 1 och 100. Gissa vilket!")
+
+# Det enda som egenglit
 antal_gissningar = 0
 
 while True:
     text = input("Din gissning: ")
     as_number = int(text)
+
+    # Denna variabel ökas för varje gissning.
     antal_gissningar += 1
 
     if as_number == n:
@@ -19,4 +23,5 @@ while True:
     if as_number > n:
         print("Fel, mitt tal är mindre. Gissa igen!")
 
-print(f"Du behövde " + str(antal_gissningar) + " gissningar.")
+# Skriv ut antalet gissningar när while-loopen avbrutits
+print(f"Du behövde {antal_gissningar} gissningar.")
