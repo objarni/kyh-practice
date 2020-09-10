39cb5c25088196fcb2502515a6fff7f3c8175f9d
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-09-10 15:04:43 +0200

    15.2

--- uppgift15_2.py
+++ uppgift15_2.py
@@ -1,2 +1,15 @@
 # 15.2 Ta in en godtycklig text (testa att copy-paste från lorumipsum.com)
 # och skriv ut hur många vokaler som finns i strängen. Tips: "a" in "abcd" är True!
+
+def count_vowels(txt):
+    vowels = "aouåeiyäö"
+    count = 0
+    for letter in txt:
+        if letter in vowels:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    txt = input("Jag kan räkna vokaler! Ge mig nått att äta: ")
+    print(f"Antal vokaler: {count_vowels(txt)}")
