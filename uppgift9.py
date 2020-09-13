d7f25c56fe43f4bd23e2b50504bcc81c712f5858
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-09-13 14:55:28 +0200

    kommentera uppg 9

--- uppgift9.py
+++ uppgift9.py
@@ -7,8 +7,11 @@
 import random
 
 
+# Jag väljer att skicka in den data som behövs till game-funktionen
 def game(number_of_questions, max_value):
     correct_answers = 0
+    # range med bara ett argument ger i = 0, 1, .., n - 1
+    # d.v.s lika många varv som number_of_questions
     for i in range(number_of_questions):
         a = random.randint(1, max_value)
         b = random.randint(1, max_value)
@@ -19,13 +22,19 @@ def game(number_of_questions, max_value):
             print("Rätt!")
             correct_answers += 1
         else:
-            print(f"Fel... Det blir {a+b}")
+            print(f"Fel... Det blir {a + b}")
             print("---")
 
         print(f"Du fick {correct_answers} av {number_of_questions} rätt.")
 
 
 if __name__ == '__main__':
+    # All data till game bestäms innan game ens startar!
+    # Obs: det går självklart att ställa dessa frågor
+    # inuti game() också, men jag tycker om att "balansera"
+    # program så inte allt ligger i en klump. Små, fokuserade
+    # block av kod = mindre risk för buggar, lättare att läsa
+    # och så vidare, ni fattar. :)
     number = int(input("Hur många frågor?"))
     max_value = int(input("Största tal?"))
     game(number, max_value)
