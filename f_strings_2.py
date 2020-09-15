da9879897a64f05495fa2b8a9ebb95386b051799
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-09-15 13:23:18 +0200

    JSON vs PY. f-strings 2

---
+++ f_strings_2.py
@@ -0,0 +1,7 @@
+tal = 5
+datum = "2020-01-01"
+value = 5.6
+
+print(f"Talet är {tal:^10} och datum {datum:^100}. Värdet är {value}")
+
+print(f"Jag vill se talen med nollor! {tal:^05}")
---
+++ json.py
@@ -0,0 +1,9 @@
+d = {
+  'name': "kalle kula",
+  'veteran': False,
+  'dates_in_order': [
+    '2001-09-11',
+    '2008-01-01',
+    '3000-10-10',
+  ]
+}
