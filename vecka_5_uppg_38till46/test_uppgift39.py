33a712a9450a2ae4a51683084e254754c95ec49d
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-10-08 14:42:19 +0200

    uppg 42

--- vecka_5_uppg_38till46/test_uppgift39.py
+++ vecka_5_uppg_38till46/test_uppgift39.py
@@ -1,4 +1,4 @@
-from vecka_5_uppg_38till46.uppgift39 import maximum
+from vecka_5_uppg_38till46.uppgift39 import maximum, summa, produkt
 
 
 def test_a_is_max():
@@ -11,3 +11,26 @@ def test_b_is_max():
 
 def test_c_is_max():
     assert maximum(5, 54, 300) == 300
+
+
+def test_summa_av_enkel_lista():
+    assert summa([1, 2, 3]) == 6
+
+
+def test_summa_av_tom_lista():
+    assert summa([]) == 0
+
+
+def test_summa_av_pos_neg_tal():
+    assert summa([50, -50]) == 0
+
+def test_produkt_av_enkel_lista():
+    assert produkt([1, 2, 3, 4]) == 24
+
+
+def test_produkt_av_tom_lista():
+    assert produkt([]) == 1
+
+
+def test_produkt_av_pos_neg_tal():
+    assert produkt([5, -5]) == -25
